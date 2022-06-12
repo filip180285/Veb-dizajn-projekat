1634c87edd188df0b7541060725472a1994ea024
Filip Kojić <filipkojic48@gmail.com>
2022-06-12 23:54:41 +0200

    Dodat meni sa zivotinjama

---
+++ srpski/js/zivotinje.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    //alert("test");
+
+    $("#pasMeni").click(function(){
+        window.location.href = "psi.html"
+    })
+
+    $("#mackaMeni").click(function(){
+        window.location.href = "macke.html"
+    })
+
+    $("#pticaMeni").click(function(){
+        window.location.href = "ptice.html"
+    })
+})
